d84b1237d3cfbeed36acbb505c2686b4be2ff1fb
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-02-06 21:24:25 -0500

    Update Delete_Same_Named_Files.py

--- Delete_Same_Named_Files.py
+++ Delete_Same_Named_Files.py
@@ -1,35 +1,28 @@
 import os
 import hashlib
 
-def delete_duplicate_files(dir_path):
-    # Create a dictionary to store files with the same name
-    files = {}
+def get_hash(filename):
+    """Calculate hash value of a file"""
+    hasher = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        buf = f.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
 
-    # Loop through all the files in the directory and subdirectories
-    for root, dirs, filenames in os.walk(dir_path):
+def delete_duplicates(directory):
+    """Delete media files with the same name and size"""
+    file_hashes = {}
+    deleted = 0
+    for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            file_path = os.path.join(root, filename)
-            file_size = os.path.getsize(file_path)
-
-            # Calculate the SHA-1 hash of the file
-            hasher = hashlib.sha1()
-            with open(file_path, 'rb') as f:
-                buf = f.read()
-                hasher.update(buf)
-            file_hash = hasher.hexdigest()
-
-            # Add the file to the dictionary if the name and size are the same
-            if filename in files:
-                for f in files[filename]:
-                    if f[0] == file_size and f[1] == file_hash:
-                        os.remove(file_path)
-                        break
-                else:
-                    files[filename].append((file_size, file_hash))
+            file_path = os.path.join(dirpath, filename)
+            file_hash = get_hash(file_path)
+            if file_hash in file_hashes:
+                os.remove(file_path)
+                deleted += 1
             else:
-                files[filename] = [(file_size, file_hash)]
+                file_hashes[file_hash] = file_path
+    print(f"Deleted {deleted} files.")
 
-if __name__ == '__main__':
-    dir_path = input('Enter the directory path: ')
-    delete_duplicate_files(dir_path)
-    print('Duplicate media files with the same name and size have been deleted.')
+directory = input("Enter the directory path: ")
+delete_duplicates(directory)
